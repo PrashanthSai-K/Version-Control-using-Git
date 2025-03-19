968d164b84e727efc36acac3de3c3e64684e1ce5
PrashanthSai-K <srisathyasai24680@gmail.com>
2025-03-19 15:07:15 +0530

    git task 10 undergoing / feature1

---
+++ task10/app.js
@@ -0,0 +1,11 @@
+const obj = {
+    id : 1,
+    name: "user",
+    college: {
+        name: "sample college",
+        state: "TN"
+    }
+}
+
+console.log("Name: ", obj.name);
+console.log("College: ", obj.college.name)
